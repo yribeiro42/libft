21231a49a34840078e15751ed42315652d8155ab
yribeiro42 <yribeiro@student.42.fr>
2016-11-10 15:38:02 +0100

    zgeg

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 22:29:05 by anonymous         #+#    #+#             */
-/*   Updated: 2016/11/09 22:49:48 by anonymous        ###   ########.fr       */
+/*   Updated: 2016/11/10 13:40:41 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,23 @@
 int		ft_atoi(const char *str)
 {
 	int	result;
-	int	sign;
+	int	negative;
 
 	result = 0;
 	while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\f'
 			|| *str == '\r' || *str == '\v')
 		str++;
 	if (*str == '-')
-		sign = -1;
-	// sign = (*str == '-') ? -1 : 1;
+		negative = 1;
 	if (*str == '-' || *str == '+')
 		str++;
-	while (ft_isdigit(*str))
+	while (*str && *str <= '9' && *str >= '0')
 	{
-		result = value * 10 + (*str - '0');
+		result = result * 10 + (*str - '0');
 		str++;
 	}
-	return (result * sign);
+	if (negative == 1)
+		return (-result);
+	else
+		return (result);
 }
