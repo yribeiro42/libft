b4541d89ed0e1fa6242ef7378603d30b76918de8
Yoann <yribeiro@student.42.fr>
2016-11-23 19:40:13 +0100

    test

--- libc/ft_strnstr.c
+++ libc/ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yribeiro <yribeiro@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anonymous <anonymous@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/15 09:50:22 by yribeiro          #+#    #+#             */
-/*   Updated: 2016/11/23 14:41:34 by yribeiro         ###   ########.fr       */
+/*   Updated: 2016/11/23 19:39:59 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 	i = 0;
 	if (ft_strlen(haystack) < ft_strlen(needle))
 		return (NULL);
-	if (!*needle || !*haystack)
+	if (!*needle)
 		return ((char *)haystack);
 	while (*haystack && n)
 	{
@@ -30,7 +30,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 		if (needle[i] == '\0')
 			return ((char *)(haystack - i + 1));
 		haystack++;
-		n--;
+		--n;
 	}
 	return (0);
 }
