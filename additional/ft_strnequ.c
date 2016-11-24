3a737639f8aee0eb99c344927a07ca7f0fe02c85
Yoann RIBEIRO <yribeiro@e2r9p11.42.fr>
2016-11-24 16:09:17 +0100

    ff

--- additional/ft_strnequ.c
+++ additional/ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: yribeiro <yribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 14:05:31 by yribeiro          #+#    #+#             */
-/*   Updated: 2016/11/15 11:19:07 by yribeiro         ###   ########.fr       */
+/*   Updated: 2016/11/24 16:02:40 by yribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 		s1++;
 		s2++;
 	}
+	if (*s1 != '\0' || *s2 != '\0')
+		return (0);
 	return (1);
 }
