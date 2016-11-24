b6cc379b8ae402489eb5928a16d0d190bd3f9c9d
Yoann <yribeiro@student.42.fr>
2016-11-24 19:25:19 +0100

    test

--- additional/ft_strsplit.c
+++ additional/ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 14:05:31 by yribeiro          #+#    #+#             */
-/*   Updated: 2016/11/24 19:14:21 by anonymous        ###   ########.fr       */
+/*   Updated: 2016/11/24 19:25:07 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,23 +53,24 @@ char			**ft_strsplit(char const *s, char c)
 	int		i;
 	int		j;
 	int		k;
-	char	**str;
+	char	**str2;
 
-	if (!s || !(str = malloc(sizeof(char *) * count_words(s, c) + 1)))
+	if (!s || !(str2 = (char **)malloc(sizeof(*str2) *
+		(count_words(s, c) + 1))))
 		return (NULL);
-	i = 0;
+	i = -1;
 	j = 0;
-	while (i++ < count_words(s, c))
+	while (++i < count_words(s, c))
 	{
 		k = 0;
-		if (!(str[i] = ft_strnew(len_words(&s[j], c) + 1)))
-			str[i] = NULL;
+		if (!(str2[i] = ft_strnew(get_word_len(&s[j], c) + 1)))
+			str2[i] = NULL;
 		while (s[j] == c)
 			j++;
 		while (s[j] != c && s[j])
-			str[i][k++] = s[j++];
-		str[i][k] = '\0';
+			str2[i][k++] = s[j++];
+		str2[i][k] = '\0';
 	}
-	str[i] = 0;
-	return (str);
+	str2[i] = 0;
+	return (str2);
 }
